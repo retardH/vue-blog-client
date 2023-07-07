18d4b2ef4de4e6a131813c4b347b60edb891af01
htetzarni <hzrni.dev@gmail.com>
2023-07-07 09:02:39 +0630

    remove moivejs store

--- src/stores/movie.js
+++
@@ -1,9 +0,0 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
-
-export const useMovieStore = defineStore('movie', {
-  state: () => ({
-    movie: null,
-    showFullVideo: false,
-  })
-})
